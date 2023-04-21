a824991a3d60f6035e859e0884436da5186d6e06
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-21 17:18:10 -0500

    formatting

--- nautobot_data_validation_engine/datasources.py
+++ nautobot_data_validation_engine/datasources.py
@@ -7,12 +7,15 @@ from nautobot.extras.registry import DatasourceContent
 def refresh_git_audit_ruleset(repository_record, job_result, delete=False):  # pylint: disable=W0613
     """Callback for repo refresh."""
     job_result.log("Successfully pulled git repo", level_choice=LogLevelChoices.LOG_SUCCESS)
-    spec = importlib.util.spec_from_file_location("custom_validators", f"{repository_record.filesystem_path}/custom_validators.py")
+    spec = importlib.util.spec_from_file_location(
+        "custom_validators", f"{repository_record.filesystem_path}/custom_validators.py"
+    )
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     if hasattr(module, "custom_validators"):
         for audit_class in module.custom_validators:
             job_result.log(f"Found class {str(audit_class.__name__)}", level_choice=LogLevelChoices.LOG_SUCCESS)
+
 
 datasource_contents = [
     (
