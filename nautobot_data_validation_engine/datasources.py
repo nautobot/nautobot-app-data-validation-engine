c82876ad3cfdff3c1d15306e4de70b17d8c5fcd1
DistantVoyager <111259311+DistantVoyager@users.noreply.github.com>
2023-06-09 10:39:38 -0400

    Changed LogLevelChoices.LOG_SUCCESS to __.LOG_INFO

--- nautobot_data_validation_engine/datasources.py
+++ nautobot_data_validation_engine/datasources.py
@@ -6,9 +6,9 @@ from nautobot_data_validation_engine.custom_validators import get_classes_from_g
 
 def refresh_git_data_compliance_rules(repository_record, job_result, delete=False):  # pylint: disable=W0613
     """Callback for repo refresh."""
-    job_result.log("Successfully pulled git repo", level_choice=LogLevelChoices.LOG_SUCCESS)
+    job_result.log("Successfully pulled git repo", level_choice=LogLevelChoices.LOG_INFO)
     for compliance_class in get_classes_from_git_repo(repository_record):
-        job_result.log(f"Found class {str(compliance_class.__name__)}", level_choice=LogLevelChoices.LOG_SUCCESS)
+        job_result.log(f"Found class {str(compliance_class.__name__)}", level_choice=LogLevelChoices.LOG_INFO)
 
 
 datasource_contents = [
