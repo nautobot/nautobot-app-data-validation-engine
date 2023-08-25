f18c0b299e7be10a2327c7a8e1ffe43425585e1c
Steven W <111259311+DistantVoyager@users.noreply.github.com>
2023-08-25 15:28:32 -0400

    Updates for nautobot 2.0.0-rc.2

--- nautobot_data_validation_engine/__init__.py
+++ nautobot_data_validation_engine/__init__.py
@@ -24,7 +24,7 @@ class NautobotDataValidationEngineConfig(NautobotAppConfig):
     description = "Provides UI to build custom data validation rules for data in Nautobot."
     base_url = "nautobot-data-validation-engine"
     required_settings = []
-    min_version = "2.0.0-rc.1"
+    min_version = "2.0.0-rc.2"
     max_version = "2.9999"
     default_settings = {}
     caching_config = {}
