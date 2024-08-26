8ce6ad9660259c28a27c2f05793b8344c8c04c15
bakebot <info@networktocode.com>
2024-08-26 20:51:40 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.3.1",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-data-validation-engine.git",
        "path": "/tmp/tmp_l3l0ifr/nautobot-app-data-validation-engine",
        "repository_path": "/tmp/tmp_l3l0ifr/nautobot-app-data-validation-engine",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@nautobot/plugin-data-validation",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_data_validation_engine",
            "verbose_name": "Data Validation Engine",
            "app_slug": "nautobot-data-validation-engine",
            "project_slug": "nautobot-app-data-validation-engine",
            "repo_url": "https://github.com/nautobot/nautobot-app-data-validation-engine",
            "base_url": "nautobot-data-validation-engine",
            "min_nautobot_version": "2.1.9",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotDataValidationEngine",
            "project_short_description": "Provides UI to build custom data validation rules for data in Nautobot",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/data-validation/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmp_l3l0ifr",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.3.1"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "7226bf600914ffd30b194ab1437cee2d9a3aa2c5",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.3.1",
        "pull_request": null,
        "post_action": [],
        "disable_post_actions": false,
        "draft": null
    }
    ```

--- nautobot_data_validation_engine/__init__.py
+++ nautobot_data_validation_engine/__init__.py
@@ -16,7 +16,7 @@ class NautobotDataValidationEngineConfig(NautobotAppConfig):
     version = __version__
     author = "Network to Code, LLC"
     description = "Provides UI to build custom data validation rules for data in Nautobot."
-    base_url = "data-validation-engine"
+    base_url = "nautobot-data-validation-engine"
     required_settings = []
     min_version = "2.1.9"
     max_version = "2.9999"
