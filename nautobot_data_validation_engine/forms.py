ed5123f216ec4099295f9607b0f6a211299920be
bakebot <info@networktocode.com>
2025-09-19 15:04:53 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "nautobot-app-v2.6.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-data-validation-engine.git",
        "path": "/tmp/tmpy3iedpyg/nautobot-app-data-validation-engine",
        "repository_path": "/tmp/tmpy3iedpyg/nautobot-app-data-validation-engine",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@nautobot/plugin-data-validation",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_data_validation_engine",
            "verbose_name": "Data Validation Engine",
            "app_slug": "nautobot-data-validation-engine",
            "project_slug": "nautobot-app-data-validation-engine",
            "repo_url": "https://github.com/nautobot/nautobot-app-data-validation-engine",
            "base_url": "nautobot-data-validation-engine",
            "camel_name": "NautobotDataValidationEngine",
            "project_short_description": "Provides UI to build custom data validation rules for data in Nautobot",
            "model_class_name": "ValidationRule",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/data-validation/en/latest",
            "_extensions": [
                "local_extensions.camel_case_to_kebab",
                "local_extensions.camel_case_to_words",
                "local_extensions.NautobotVersions"
            ],
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpy3iedpyg",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "nautobot-app-v2.6.0"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.UPDATE_OR_CREATE",
        "post_actions": [],
        "baked_commit_ref": "a4f87d6db0205535a0f477d34a9ada25141e5d2a",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "nautobot-app-v2.6.0",
        "pull_request": "update-or-create",
        "post_action": [],
        "disable_post_actions": true,
        "draft": null
    }
    ```

--- nautobot_data_validation_engine/forms.py
+++ nautobot_data_validation_engine/forms.py
@@ -1,6 +1,7 @@
 """Forms for nautobot_data_validation_engine."""
 
 from django import forms
+from nautobot.apps.constants import CHARFIELD_MAX_LENGTH
 from nautobot.apps.forms import NautobotBulkEditForm, NautobotFilterForm, NautobotModelForm, TagsBulkEditFormMixin
 
 from nautobot_data_validation_engine import models
@@ -20,7 +21,7 @@ class ValidationRuleBulkEditForm(TagsBulkEditFormMixin, NautobotBulkEditForm):  
     """ValidationRule bulk edit form."""
 
     pk = forms.ModelMultipleChoiceField(queryset=models.ValidationRule.objects.all(), widget=forms.MultipleHiddenInput)
-    description = forms.CharField(required=False)
+    description = forms.CharField(required=False, max_length=CHARFIELD_MAX_LENGTH)
 
     class Meta:
         """Meta attributes."""
--- nautobot_data_validation_engine/views.py
+++ nautobot_data_validation_engine/views.py
@@ -1,6 +1,8 @@
 """Views for nautobot_data_validation_engine."""
 
 from nautobot.apps.views import NautobotUIViewSet
+from nautobot.apps.ui import ObjectDetailContent, ObjectFieldsPanel, ObjectTablePanel, SectionChoices
+from nautobot.core.templatetags import helpers
 
 from nautobot_data_validation_engine import filters, forms, models, tables
 from nautobot_data_validation_engine.api import serializers
@@ -17,3 +19,34 @@ class ValidationRuleUIViewSet(NautobotUIViewSet):
     queryset = models.ValidationRule.objects.all()
     serializer_class = serializers.ValidationRuleSerializer
     table_class = tables.ValidationRuleTable
+
+    # Here is an example of using the UI  Component Framework for the detail view.
+    # More information can be found in the Nautobot documentation:
+    # https://docs.nautobot.com/projects/core/en/stable/development/core/ui-component-framework/
+    object_detail_content = ObjectDetailContent(
+        panels=[
+            ObjectFieldsPanel(
+                weight=100,
+                section=SectionChoices.LEFT_HALF,
+                fields="__all__",
+                # Alternatively, you can specify a list of field names:
+                # fields=[
+                #     "name",
+                #     "description",
+                # ],
+                # Some fields may require additional configuration, we can use value_transforms
+                # value_transforms={
+                #     "name": [helpers.bettertitle]
+                # },
+            ),
+            # If there is a ForeignKey or M2M with this model we can use ObjectTablePanel
+            # to display them in a table format.
+            # ObjectTablePanel(
+                # weight=200,
+                # section=SectionChoices.RIGHT_HALF,
+                # table_class=tables.ValidationRuleTable,
+                # You will want to filter the table using the related_name
+                # filter="validationrules",
+            # ),
+        ],
+    )
